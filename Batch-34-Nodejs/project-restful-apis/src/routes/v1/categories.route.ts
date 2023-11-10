2185b462d826be157b2db6140d89a3b03b7c8ef3
nhan <nhannn87dn@gmail.com>
2023-11-10 19:51:53 +0700

    up

--- Batch-34-Nodejs/project-restful-apis/src/routes/v1/categories.route.ts
+++ Batch-34-Nodejs/project-restful-apis/src/routes/v1/categories.route.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import categoriesController from '../../controllers/categories.controller';
 import authMiddleware from '../../middleware/auth.middleware';
-import categoriesTypeORMController from '../../controllers/categoriesTypeORM.controller';
 /***
  * Route chỉ làm nhiệm vụ định tuyến
  * Mapping request giữa client với Server
@@ -11,11 +10,6 @@ const router = express.Router();
 
 //Get All categories from DB
 router.get('/', categoriesController.getAll);
-//Test typeORM
-router.get('/typeorm/getall', categoriesTypeORMController.getAll);
-
-//Test typeORM
-router.get('/typeorm/:id', categoriesTypeORMController.getItemById);
 
 //get user by ID
 //Gắn middleware vào để check id có phải là số không
@@ -24,21 +18,20 @@ router.get('/:id', categoriesController.getItemById);
 //Create a new user
 //Check Token
 //Check xem user có quyền User không ?
-// router.post('/', authMiddleware.checkToken, authMiddleware.checkAuthorize(["User","Admin"]), categoriesController.createItem);
-router.post('/', categoriesTypeORMController.createItem);
+router.post('/', authMiddleware.checkToken, authMiddleware.checkAuthorize(["User","Admin"]), categoriesController.createItem);
+
 /**
  * Update a user by ID
  * PATH /api/v1//:id
  */
-//router.patch('/:id', authMiddleware.checkToken,  categoriesController.updateItem);
-router.patch('/:id', categoriesTypeORMController.updateItem);
+router.patch('/:id', authMiddleware.checkToken,  categoriesController.updateItem);
+
 
 /**
  * Delete a user by ID
  * DELETE /api/v1//:id
  */
-//router.delete('/:id', authMiddleware.checkToken, authMiddleware.checkAuthorize(["Admin"]), categoriesController.deleteItem);
-router.delete('/:id',  categoriesTypeORMController.deleteItem);
+router.delete('/:id', authMiddleware.checkToken, authMiddleware.checkAuthorize(["Admin"]), categoriesController.deleteItem);
 
 //Xuất router ra
 export default router;
---
+++ Batch-34-Nodejs/project-restful-apis/src/tests/sample.test.ts
@@ -0,0 +1,7 @@
+import * as request from 'supertest'
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
