5073febba4cb53f40c6447cad355895b8ad948c9
nhan <nhannn87dn@gmail.com>
2024-08-02 19:07:02 +0700

    up

---
+++ Batch-39/nodejs-backend-api/src/databases/seed/db.ts
@@ -0,0 +1,115 @@
+import { categories } from './../../../../../Batch-37-38/project-restful-apis/src/seeds/category';
+//File này dùng để tạo dữ liệu cho database
+
+import mongoose from 'mongoose'
+import {globalConfig} from '../../constants/configs'
+import Category from '../../models/categories.model';
+import { faker } from '@faker-js/faker';
+import Brand from '../../models/brands.model';
+import Product from '../../models/products.model';
+
+const mongooseDbOptions = {
+  autoIndex: true, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(globalConfig.MONGODB_URL as string, mongooseDbOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    //should listen app here
+  })
+  .catch((err) => {
+    console.error('Failed to Connect to MongoDB', err);
+  });
+
+  const brands = [
+    {
+        brand_name: "Trek",
+        description: "High-quality bikes for all terrains",
+        slug: "trek"
+    },
+    {
+        brand_name: "Giant",
+        description: "Specializing in road and mountain bikes",
+        slug: "giant"
+    },
+    {
+        brand_name: "Specialized",
+        description: "Innovative designs for cycling enthusiasts",
+        slug: "specialized"
+    },
+    {
+        brand_name: "Cannondale",
+        description: "Known for its performance-oriented bicycles",
+        slug: "cannondale"
+    },
+    {
+        brand_name: "Scott",
+        description: "Offers a wide range of bicycles for various purposes",
+        slug: "scott"
+    }
+];
+
+
+const runDB = async ()=>{
+  console.log('runDB running....');
+  //tạo mới 5 danh mục ngẫu nhiên
+
+  // for (let index = 1; index < 6; index++) {
+    
+  //   const category = new Category({
+  //     category_name: faker.commerce.department()+index,
+  //     description: faker.lorem.text(),
+  //     slug: faker.lorem.slug()+index,
+  //   });
+  //   //Đến bước nó mới chính thức ghi xuống DB
+  //   await category.save();
+  //   console.log('Tạo danh mục thành công....');
+  // }
+
+  //Tạo brands từ mảng có sẵn
+ // await Brand.insertMany(brands)
+  
+ const currentBrands = await Brand.find();
+ const currentCategories = await Category.find();
+
+ 
+   for (let i = 1; i <= 15; i++) {
+
+    let productName = faker.commerce.productName()+i;
+    
+    const brand = currentBrands[Math.floor(Math.random() * currentBrands.length)];
+    const category = currentCategories[Math.floor(Math.random() * currentCategories.length)];
+
+    const fakeProduct = {
+      productName: productName,
+      price: faker.commerce.price({ min: 100, max: 1200 }),
+      discount: faker.number.int({ min: 1, max: 50 }),
+      category: category._id,
+      brandId: brand._id,
+      description: faker.commerce.productDescription(),
+      model_year: faker.helpers.fromRegExp('2[0-9]{3}'),
+      stock: faker.number.int({ min: 1, max: 200 }), // Thêm trường stock
+      thumbnail: 'https://picsum.photos/400/400', // Thêm trường thumbnail
+      slug: faker.helpers.slugify(productName), // Tạo slug từ productName
+    }
+   
+    const product = new Product(fakeProduct);
+    await product.save();
+    console.log(`Create Product ${i} successfully !`);
+    
+  }
+}
+
+  
+
+try {
+  runDB()
+} catch (error) {
+  console.log(error);
+}
--- Batch-39/nodejs-backend-api/src/models/brands.model.ts
+++ Batch-39/nodejs-backend-api/src/models/brands.model.ts
@@ -2,37 +2,14 @@ import { Schema, model } from 'mongoose';
 
 /* Khởi tạo một Schema */
 
-const productSchema = new Schema({
-  product_name: {
+const brandSchema = new Schema({
+  brand_name: {
     type: String,
     require: true, //mặc định true, nếu bạn ko liệt kê vào
-    maxLength: 255, //Tối đa 255 kí tự
+    minLength: [4, "Tên danh mục tối thiểu phải 4 kí tự"],
+    maxLength: 50, //Tối đa 50 kí tự
     unique: true, //chống trùng lặp tên danh mục
     trim: true, // tự động cắt kí tự trắng trước/sau vd: "   Laptop " ==> "Laptop"
-  },
-  price: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  discount: {
-    type: Number,
-    min: 0,
-    max: 70,
-    default: 0
-  },
-  category: {
-    type: Schema.Types.ObjectId, //_id
-    ref: 'Category',
-    required: true
-  },
-  brandId: {
-    type: Schema.Types.ObjectId, //_id
-    ref: 'Brand',
-    required: true
-  },
-  model_year: {
-    type: Number
   },
   description: {
     type: String,
@@ -40,24 +17,15 @@ const productSchema = new Schema({
     maxLength: 500, //Tối đa 500 kí tự
     trim: true, // tự động cắt kí tự trắng trước/sau vd: "   Laptop " ==> "Laptop"
   },
-  thumbnail: {
-    type: String,
-    require: false,
-  },
-  stock: {
-    type: Number,
-    default: 0,
-    min: 0
-  },
   /**
    * Vì sao cần slug ---> để SEO
-   * category_name: Điện thoại
+   * brand_name: Điện thoại
    * slug: dien-thoai
    */
   slug: {
     type: String,
     require: true, //mặc định true, nếu bạn ko liệt kê vào
-    maxLength: 255, //Tối đa 50 kí tự
+    maxLength: 50, //Tối đa 50 kí tự
     unique: true, //chống trùng lặp tên danh mục
     trim: true, // tự động cắt kí tự trắng trước/sau vd: "   Laptop " ==> "Laptop"
   },
@@ -65,33 +33,15 @@ const productSchema = new Schema({
     type: Number,
     default: 50, //giá trị mặc định khi ko điền,
     min: 1, //giá trị tối thiểu chấp nhận là 1
-  },
-  /* SP bán nổi bật */
-  isBest: {
-    type: Boolean,
-    require: false,
-    default: false
-  },
-  /* SP mới về */
-  isNew: {
-    type: Boolean,
-    require: false,
-    default: false
-  },
-  /* Show sp ra trang chủ */
-  isShowHome: {
-    type: Boolean,
-    require: false,
-    default: false
-  },
+  }
 },
 {
   timestamps: true, //Tạo tự động thêm 2 trường createAt, updateAt
-  //collection: 'category', //Tên collection Cố định theo tên bạn đặt
+  //collection: 'categories'
 }
 )
 
 //Export một Model
 
-const Product = model('Product', productSchema);
-export default Product;
+const Brand = model('Brand', brandSchema);
+export default Brand;
