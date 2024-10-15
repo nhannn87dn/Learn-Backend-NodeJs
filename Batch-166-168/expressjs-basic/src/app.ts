dc46d006595dc7aba7c0dae32ab6ec2388da85b3
nhan <nhannn87dn@gmail.com>
2024-10-15 20:38:37 +0700

    up

--- Batch-166-168/expressjs-basic/src/app.ts
+++ Batch-166-168/expressjs-basic/src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import createError from 'http-errors'
 import categoryRoute from './routes/category.route'
 const app = express();
 
@@ -19,5 +20,24 @@ app.get("/", (req, res) => {
 //==== DINH NGHIA ROUTES ==== //
 app.use('/api/v1/categories', categoryRoute);
 
-//
+
+
+//=== HANDLE ERRORS BEGIN HERE ====//
+// Lưu ý: Đặt trước phần export, và sau các route khác
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction)=>{
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction)=>{
+ 
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({ 
+    statusCode: statusCode, 
+    message: err.message 
+  });
+});
+
 export default app
