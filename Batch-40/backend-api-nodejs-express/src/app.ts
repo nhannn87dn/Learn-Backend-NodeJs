e824e488f98b43529a01339fd3878f0ad3a65894
nhannn87dn <nhannn87dn@gmail.com>
2025-02-11 19:42:07 +0700

    step 4

--- Batch-40/backend-api-nodejs-express/src/app.ts
+++ Batch-40/backend-api-nodejs-express/src/app.ts
@@ -1,5 +1,6 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import categoriesRouter from './routes/v1/categories.route'
+import createError from 'http-errors';
 const app = express();
 
 /** -------|| BEGIN REGISTER ROUTES || --------- */
@@ -10,4 +11,18 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/v1', categoriesRouter);
 /** -------|| END REGISTER ROUTES || --------- */
 
+// NO EDIT BEGIN HERE
+/** -------|| BEGIN HANDLE ERRORS || --------- */
+// catch 404 and forward to error handler
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  next(createError(404));
+});
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({ 
+    statusCode: statusCode, 
+    message: err.message
+  });
+});
+/** -------|| END HANDLE ERRORS || --------- */
 export default app;
--- Batch-40/backend-api-nodejs-express/src/routes/v1/categories.route.ts
+++ Batch-40/backend-api-nodejs-express/src/routes/v1/categories.route.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import createError from 'http-errors';
 
 const router = express.Router();
 
+const categories = [
+    {
+        id: 1,
+        name: 'Category 1'
+    },
+    {
+        id: 2,
+        name: 'Category 2'
+    }
+]
+
+// Get All Categories
 // GET /api/v1/categories
 router.get('/categories', (req, res) => {
-  res.send('categories');
+  //res.send('Retrieve all categories');
+  res.status(200).json(categories);
+});
+
+// Get Category by Id
+router.get('/categories/:id', (req, res) => {
+    const {id} = req.params;
+    const category = categories.find(category => category.id == Number(id));
+    //Nếu không tìm thấy category thì trả về lỗi 404
+    if(!category){
+       ///throw new Error('Category not found');
+        throw createError(400, 'Category not found');
+    }
+    res.status(200).json(category);
+ 
 });
 
 export default router;
