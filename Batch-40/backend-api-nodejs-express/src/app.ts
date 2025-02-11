29478dfa9b6b9fe3ba1245dbbf17706264ad9ce4
nhannn87dn <nhannn87dn@gmail.com>
2025-02-11 18:34:06 +0700

    step 2

--- Batch-40/backend-api-nodejs-express/src/app.ts
+++ Batch-40/backend-api-nodejs-express/src/app.ts
@@ -1,12 +1,10 @@
 import express, {Request, Response} from 'express';
 
 const app = express();
-const PORT = 3000;
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+export default app;
