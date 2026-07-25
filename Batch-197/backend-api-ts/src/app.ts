997aaf5a77c6ea5bd3b1e5ca58ffc0852e3c062c
nhannn87dn <nhannn87dn@gmail.com>
2026-07-25 20:00:17 +0700

    add route

--- Batch-197/backend-api-ts/src/app.ts
+++ Batch-197/backend-api-ts/src/app.ts
@@ -2,6 +2,10 @@ import express, { type Express, type Request, type Response } from 'express';
 import categoriesRouter from "./routes/v1/categories.route"
 const app: Express = express();
 
+// Middleware để parse JSON body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
--- Batch-197/backend-api-ts/src/routes/v1/categories.route.ts
+++ Batch-197/backend-api-ts/src/routes/v1/categories.route.ts
@@ -26,12 +26,41 @@ router.get('/:id', (req: Request, res: Response) => {
 router.post('/', (req: Request, res: Response) => {
     // Lấy dữ liệu từ body của request
     const payload = req.body;
+    console.log('<<=== 🚀 payload ===>>',payload);
     //response lại cho client
     res
     .status(201)
     .json({ 
         message: 'create a new category',
         data: payload 
+    });
+});
+
+// PUT /api/v1/categories/:id - update a category by id
+router.put('/:id', (req: Request, res: Response) => {
+    //lấy id cần update
+    const { id } = req.params;
+    // Lấy dữ liệu từ body của request
+    const payload = req.body;
+    console.log('<<=== 🚀 payload ===>>',payload);
+    //response lại cho client
+    res
+    .status(200)
+    .json({ 
+        message: 'update a category by id',
+        id: id,
+        data: payload 
+    });
+});
+
+// DELETE /api/v1/categories/:id - delete a category by id
+router.delete('/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+    res
+    .status(200)
+    .json({ 
+        message: 'delete a category by id',
+        id: id
     });
 });
 
