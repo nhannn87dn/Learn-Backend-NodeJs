de9fc1c6c3c566f5bf1af9f530217cec78c6a420
nhannn87dn <nhannn87dn@gmail.com>
2026-07-28 19:31:46 +0700

    add service layer

--- Batch-197/backend-api-ts/src/controllers/categories.controller.ts
+++ Batch-197/backend-api-ts/src/controllers/categories.controller.ts
@@ -1,18 +1,15 @@
 import { type Request, type Response } from 'express';
-
-import fs from 'node:fs';
 import { TCategory } from '../types/category';
 import { readFile, writeFile } from '../helpers/file.helper';
+import categoriesService from '../services/categories.service';
+
 const fileName = 'src/database/data.json';
 
 //Get All Categories
 const getAllCategories = (req: Request, res: Response) => {
 
-    // Lấy dữ liệu từ file data.json
-    // const data = fs.readFileSync(fileName, { encoding: 'utf-8', flag: 'r' });
-    // const categories: TCategory[] = JSON.parse(data);
-    const categories: TCategory[] = readFile(fileName);
-    console.log('<<=== 🚀 categories ===>>',categories);
+    //Kết nối với service để lấy dữ liệu categories
+   const categories = categoriesService.findAll();
 
     res
     .status(200)
@@ -27,28 +24,13 @@ const getAllCategories = (req: Request, res: Response) => {
 const getCategoryById = (req: Request, res: Response) => {
     const { id } = req.params;
 
-    if (!id) {
-        throw new Error('Category id is required');
-    }
-    
-    // Lấy dữ liệu từ file data.json
-   const categories: TCategory[] = readFile(fileName);
-
-    console.log('<<=== 🚀 categories ===>>',categories);
-
-    // Tìm category theo id
-    const category = categories.find((c)=> c.id === parseInt(id as string));
-
-    console.log('<<=== 🚀 category ===>>',category);
-    if (!category) {
-        throw new Error(`Category with id ${id} not found`);
-    }
+   const category = categoriesService.findById(parseInt(id as string));
 
     res
     .status(200)
     .json({ 
         statusCode: 200,
-        message: 'Get category by id',
+        message: 'successfully get category by id',
         data: category
     });
 };
@@ -59,20 +41,7 @@ const createCategory = (req: Request, res: Response) => {
     const payload = req.body;
     console.log('<<=== 🚀 payload ===>>',payload);
 
-    // Lấy dữ liệu từ file data.json
-    const categories: TCategory[] = readFile(fileName);
-
-    //thêm category mới vào mảng categories
-    categories.push({
-        id: categories.length + 1, //tự động tăng id
-        name: payload.name,
-        description: payload.description
-    });
-
-    // Ghi dữ liệu mới vào file data.json
-    //fs.writeFileSync(fileName, JSON.stringify(categories, null, 2), { encoding: 'utf-8' });
-    writeFile(fileName, categories);
-
+    const category = categoriesService.create(payload);
     //response lại cho client
 
     res
@@ -80,7 +49,7 @@ const createCategory = (req: Request, res: Response) => {
     .json({ 
         statusCode: 201,
         message: 'create a new category',
-        data: payload //response lại cho client category vừa được tạo
+        data: category //response lại cho client category vừa được tạo
     });
 };
 
@@ -91,37 +60,8 @@ const updateCategoryById = (req: Request, res: Response) => {
     // Lấy dữ liệu từ body của request
     const payload = req.body;
     console.log('<<=== 🚀 payload ===>>',payload);
-
-    // Lấy dữ liệu từ file data.json
-    const categories: TCategory[] = readFile(fileName);
-
-    console.log('<<=== 🚀 categories ===>>',categories);
-
-    //tìm category cần update voi Id
-    let category = categories.find((c)=> c.id === parseInt(id as string));
-    if (!category) {
-        throw new Error(`Category with id ${id} not found`);
-    }
-
-    //cập nhật category và lưu lại
-   // 3. Cập nhật mảng categories
-    const updatedCategories = categories.map((c) => {
-        if (c.id === parseInt(id as string)) {
-            return {
-                ...c,
-                name: payload.name || c.name,
-                description: payload.description || c.description,
-            };
-        }
-        return c;
-    });
-
-    // Ghi dữ liệu mới vào file data.json
-    writeFile(fileName, updatedCategories);
-
-    //response lại cho client category vừa được update
-    const categoryUpdated = updatedCategories.find((c)=> c.id === parseInt(id as string));
-
+    const categoryUpdated = categoriesService.updateById(parseInt(id as string), payload);
+    
     res
     .status(200)
     .json({ 
@@ -135,26 +75,13 @@ const updateCategoryById = (req: Request, res: Response) => {
 const deleteCategoryById = (req: Request, res: Response) => {
     const { id } = req.params;
 
-    // Lấy dữ liệu từ file data.json
-    const categories: TCategory[] = readFile(fileName);
-
-    //tìm category cần delete voi Id
-    const category = categories.find((c)=> c.id === parseInt(id as string));
-    if (!category) {
-        throw new Error(`Category with id ${id} not found`);
-    }
-
-    // 3. Xóa category khỏi mảng categories
-    const updatedCategories = categories.filter((c) => c.id !== parseInt(id as string));    
-
-    // Ghi dữ liệu mới vào file data.json
-    writeFile(fileName, updatedCategories);
-
+    const category = categoriesService.deleteById(parseInt(id as string));
+    
     res
     .status(200)
     .json({ 
         statusCode: 200,
-        message: 'delete a category by id',
+        message: 'delete category successfully',
         data: category //trả lại cho client category vừa được delete
     });
 };
---
+++ Batch-197/backend-api-ts/src/services/categories.service.ts
@@ -0,0 +1,117 @@
+import { readFile, writeFile } from '../helpers/file.helper';
+import { TCategory } from '../types/category';
+
+
+const fileName = 'src/database/data.json';
+
+
+//Get All Categories
+const findAll = ()=>{
+     // Lấy dữ liệu từ file data.json
+    // const data = fs.readFileSync(fileName, { encoding: 'utf-8', flag: 'r' });
+    // const categories: TCategory[] = JSON.parse(data);
+    const categories: TCategory[] = readFile(fileName);
+    console.log('<<=== 🚀 categories ===>>',categories);
+    return categories;
+}
+//Get Category by ID'
+
+const findById = (id: number)=>{
+     // Lấy dữ liệu từ file data.json
+   const categories: TCategory[] = readFile(fileName);
+
+    console.log('<<=== 🚀 categories ===>>',categories);
+
+    // Tìm category theo id
+    const category = categories.find((c)=> c.id === id);
+
+    console.log('<<=== 🚀 category ===>>',category);
+    if (!category) {
+        throw new Error(`Category with id ${id} not found`);
+    }
+
+    return category;
+}
+
+//create a new category
+const create = (payload: TCategory)=>{
+    // Lấy dữ liệu từ file data.json
+    const categories: TCategory[] = readFile(fileName);
+
+    //thêm category mới vào mảng categories
+    const newCategory: TCategory = {
+        id: categories.length + 1, //tự động tăng id
+        name: payload.name,
+        description: payload.description
+    };
+    categories.push(newCategory);
+    
+
+    // Ghi dữ liệu mới vào file data.json
+    //fs.writeFileSync(fileName, JSON.stringify(categories, null, 2), { encoding: 'utf-8' });
+    writeFile(fileName, categories);
+    return newCategory
+}
+
+//update a category by id
+
+const updateById = (id: number, payload: TCategory)=>{
+    // Lấy dữ liệu từ file data.json
+        const categories: TCategory[] = readFile(fileName);
+    
+        console.log('<<=== 🚀 categories ===>>',categories);
+    
+        //tìm category cần update voi Id
+        let category = categories.find((c)=> c.id === id);
+        if (!category) {
+            throw new Error(`Category with id ${id} not found`);
+        }
+    
+        //cập nhật category và lưu lại
+       // 3. Cập nhật mảng categories
+        const updatedCategories = categories.map((c) => {
+            if (c.id === id) {
+                return {
+                    ...c,
+                    name: payload.name || c.name,
+                    description: payload.description || c.description,
+                };
+            }
+            return c;
+        });
+    
+        // Ghi dữ liệu mới vào file data.json
+        writeFile(fileName, updatedCategories);
+    
+        //response lại cho client category vừa được update
+        const categoryUpdated = updatedCategories.find((c)=> c.id === id);
+    return categoryUpdated
+}
+
+//delete a category by id
+const deleteById = (id: number)=>{
+    // Lấy dữ liệu từ file data.json
+    const categories: TCategory[] = readFile(fileName);
+
+    //tìm category cần delete voi Id
+    const category = categories.find((c)=> c.id === id);
+    if (!category) {
+        throw new Error(`Category with id ${id} not found`);
+    }
+
+    // 3. Xóa category khỏi mảng categories
+    const updatedCategories = categories.filter((c) => c.id !== id);    
+
+    // Ghi dữ liệu mới vào file data.json
+    writeFile(fileName, updatedCategories);
+    return category 
+}
+
+
+export default {
+    findAll,
+    findById,
+    create,
+    updateById,
+    deleteById
+}
