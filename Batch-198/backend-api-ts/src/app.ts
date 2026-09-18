6de23e4f097140922d74ee59607699790313b75c
nhannn87dn <nhannn87dn@gmail.com>
2026-09-18 19:27:48 +0700

    resoure API

--- Batch-198/backend-api-ts/src/app.ts
+++ Batch-198/backend-api-ts/src/app.ts
@@ -5,6 +5,11 @@ import studentsRouter from "./routes/students.route";
 const app: Express = express();
 const PORT = ENV.PORT;
 
+//Parse JSON và URL-encoded data từ request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Express + TypeScript Server" });
 });
--- Batch-198/backend-api-ts/src/routes/students.route.ts
+++ Batch-198/backend-api-ts/src/routes/students.route.ts
@@ -2,9 +2,45 @@ import express, { Router } from "express";
 
 const router: Router = express.Router();
 
-// GET /api/students
+// GET /api/students - get All Students
 router.get("/", (req, res) => {
   res.status(200).json({ message: "List of students" });
+});
+
+// GET /api/students/:id - get Student by ID
+router.get("/:id", (req, res) => {
+    //Lấy id trong params của request
+  const studentId = req.params.id;
+  res.status(200).json({ message: `Details of student with ID: ${studentId}` });
+});
+
+// POST /api/students - create a new student
+router.post("/", (req, res) => {
+  // Lấy dữ liệu từ body của request
+  const newStudent = req.body;
+  console.log('<<=== 🚀 newStudent ===>>',newStudent);
+  //Thêm mới 1 cái gì thành công
+  //trả về mã là 201
+  res.status(201).json({ 
+    message: "New student created", 
+    student: newStudent 
+    });
+});
+
+// PUT /api/students/:id - update a student by ID
+router.put("/:id", (req, res) => {
+    // Lấy id trong params của request
+  const studentId = req.params.id;
+  // Lấy dữ liệu từ body của request để cập nhật
+  const updatedStudent = req.body;
+  res.status(200).json({ message: `Student with ID: ${studentId} updated`, student: updatedStudent });
+});
+
+// DELETE /api/students/:id - delete a student by ID
+router.delete("/:id", (req, res) => {
+    // Lấy id trong params của request
+  const studentId = req.params.id;
+  res.status(200).json({ message: `Student with ID: ${studentId} deleted` });
 });
 
 export default router;
